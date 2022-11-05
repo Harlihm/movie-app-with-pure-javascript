35f09cb4ab19e5dcd629fa598c6bbea92883ae33
Harlihm <107759287+Harlihm@users.noreply.github.com>
2022-11-05 03:36:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 const API_KEY = 'a791ad0f144ddfcc6ebe3aa512ef4f43';
 // const url = "https://api.themoviedb.org/3/movie/550?api_key=a791ad0f144ddfcc6ebe3aa512ef4f43";
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTrending: `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`,
+    fetchTopRated: `https://api.themoviedb.org/3//movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `https://api.themoviedb.org/3//discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `https://api.themoviedb.org/3//discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `https://api.themoviedb.org/3//discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `https://api.themoviedb.org/3//discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `https://api.themoviedb.org/3//discover/movie?api_key=${API_KEY}&with_genres=99`,
 };
 
 const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a791ad0f144ddfcc6ebe3aa512ef4f43&page=1";
@@ -20,13 +19,14 @@ const IMAGE_PATH = "https://image.tmdb.org/t/p/w1280/";
         const form=document.getElementById('form');
         const search=document.getElementById('search');
         const main=document.getElementById('main');
-        const main2=document.getElementById('mainII');
+        const main2=document.getElementById('main2');
+        var imgclick=document.getElementsByClassName('movie');
 
-
+      
 
 const getMovies = async () => {
     let url = API_URL
-const response = await fetch(`https://api.themoviedb.org/3//discover/tv?api_key=${API_KEY}&with_networks=213`)
+const response = await fetch(url)
 const data = await response.json();
 
 return data;
@@ -35,12 +35,12 @@ return data;
 };
 
 
-getMovies()
+getMovies(API_URL)
   .then ( 
     data =>
      {
+      
       displayMovies(data.results)
-      displayMoviesInfo(data.results)
       function displayMovies(movies) {
         main.innerHTML=''
         movies.forEach((movie) => {
@@ -50,32 +50,21 @@ getMovies()
             moviesElement.innerHTML=`
             
             <img class="mainimg" src="${IMAGE_PATH + poster_path}" alt="${title}">
+            <div class="originals">
+            <h3>${title}</h3>
+            <p class="sypnosis">${overview}</p>
+            <span>RATING ${vote_average}</span>
+            </div>
+          
            
             `
+
             main.appendChild(moviesElement)
-            
+
         });
        }
-       function displayMoviesInfo(movies) {
-        main2.innerHTML=''
-        movies.forEach((movie) => {
-            const {title,poster_path,vote_average,overview}= movie
-            const moviesElement = document.createElement('div')
-            moviesElement.classList.add('movie')
-            moviesElement.innerHTML=`
-            
-           <div class="originals">
-           <h3>${title}</h3>
-           <p class="sypnosis">t${overview}</p>
-           <span> ${vote_average}</span>
-           </div>
-         
-           
-            `
-            main2.appendChild(moviesElement)
-            
-        });
-       }
+
+     
 
         form.addEventListener('submit' , (e) =>{
             e.preventDefault()
@@ -88,11 +77,66 @@ getMovies()
             }
           })
 
+          
+          imgclick.addEventListener('click', ()=>{
+            console.log("clicked")
+             
+           })
          
     }  
     
   )
   .catch (err => ('rejected', err.message));
+
+
+  const getTrendng = async (url) => {
+const response = await fetch(url)
+const data = await response.json();
+
+return data;
+
+
+};
+
+
+getTrendng(requests.fetchTrending)
+  .then ( 
+    data =>
+     {
+      
+      displayMovies(data.results)
+      function displayMovies(movies) {
+        main2.innerHTML=''
+        movies.forEach((movie) => {
+            const {title,poster_path,vote_average,overview}= movie
+            const moviesElement = document.createElement('div')
+            moviesElement.classList.add('movie')
+            moviesElement.innerHTML=`
+            
+            <img class="mainimg" src="${IMAGE_PATH + poster_path}" alt="${title}">
+            <div class="originals">
+            <h3>${title}</h3>
+            <p class="sypnosis">${overview}</p>
+            <span>RATING ${vote_average}</span>
+            </div>
+          
+           
+            `
+
+            main2.appendChild(moviesElement)
+
+        });
+       }
+
+    
+         
+    }  
+    
+  )
+  .catch (err => ('rejected', err.message));
+  
+
+  
 
 
 
